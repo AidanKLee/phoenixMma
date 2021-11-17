5901d9e9852c2b8660c8736fa02056505efb920e
Aidan Lee <aidankglee@googlemail.com>
2021-11-17 11:41:24 +0000

    Stopped scrolling and blur on menu.

--- resources/script/menu.js
+++ resources/script/menu.js
@@ -3,7 +3,15 @@ const menuClsBtn = document.getElementById('menuNavCloseBtn');
 const menu = document.getElementById('menu');
 const menuNav = document.getElementById('menuNav');
 
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+
 const openMenu = () => {
+
+    body.classList.add('stopScrolling');
 
     menu.classList.remove('fadeOut');
     menuNav.classList.remove('menuOut');
@@ -11,9 +19,17 @@ const openMenu = () => {
     menu.classList.add('fadeIn');
     menuNav.classList.add('menuIn');
 
+    header.classList.add('blur');
+    main.classList.add('blur');
+    footer.classList.add('blur');
+
 }
 
 const closeMenu = () => {
+
+    header.classList.remove('blur');
+    main.classList.remove('blur');
+    footer.classList.remove('blur');
 
     menu.classList.remove('fadeIn');
     menuNav.classList.remove('menuIn');
@@ -21,7 +37,9 @@ const closeMenu = () => {
     menuNav.classList.add('menuOut');
     setTimeout(function() {
         menu.style.display = 'none';
-}, 990);
+    }, 990);
+
+    body.classList.remove('stopScrolling');
 
 }
 
